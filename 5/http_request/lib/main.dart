6f546061223c8eb3e7affbd84288a2219acc704a
Stanislav Ilin <youngfrezyx@gmail.com>
2024-09-05 03:35:01 +0500

    feat(http) Добавить раздел с базовым http запросом

---
+++ 5/http_request/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Map<String, dynamic>? _userData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _fetchUsersList,
+              child: const Text('Получить пользователя'),
+            ),
+            const SizedBox(height: 8),
+            Text(_userData != null ? _userData!.toString() : 'Данных нет')
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _fetchUsersList() async {
+    final url = Uri.parse('https://jsonplaceholder.typicode.com/users/1');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body) as Map<String, dynamic>;
+      setState(() => _userData = data);
+      return;
+    }
+  }
+}
