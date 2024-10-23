f78ad510317e8426483631e1205c9ef1f0fad1bd
Stanislav Ilin <youngfrezyx@gmail.com>
2024-10-24 02:45:09 +0500

    feat(http) Добавить пример Web-сокетов

--- 5/backend/routes/ws/write/index.dart
+++ 5/backend/routes/ws/write/index.dart
@@ -4,16 +4,8 @@ import 'package:dart_frog_web_socket/dart_frog_web_socket.dart';
 Future<Response> onRequest(RequestContext context) async {
   final handler = webSocketHandler((channel, protocol) async {
     channel.stream.listen((message) {
-      // Handle incoming client messages.
-      print(message);
+      channel.sink.add('Получил сообщение "${message?.toString() ?? ''}"');
     });
-
-    for (var i = 0; i < 100; i++) {
-      await Future.delayed(Duration(milliseconds: 5000));
-
-      // Send a message back to the client.
-      channel.sink.add('hi');
-    }
   });
   return handler(context);
 }
---
+++ 5/web_socket_flutter/android/app/src/main/kotlin/com/example/web_socket_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.web_socket_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
