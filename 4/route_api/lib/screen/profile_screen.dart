956b23cdfa62118d0c4fd24dd2548e0760d90c8b
petrovyuri <y.petrov@friflex.com>
2024-12-16 18:32:07 +0300

    fix

--- 4/route_api/lib/screen/profile_screen.dart
+++ 4/route_api/lib/screen/profile_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:route_api/example/my_route_delegate.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -8,15 +7,6 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('ProfileScreen')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            (Router.of(context).routerDelegate as MyRouterDelegate)
-                .setNewRoutePath('/profile/detail');
-          },
-          child: const Text('Перейти в DetailScreen'),
-        ),
-      ),
     );
   }
 }
