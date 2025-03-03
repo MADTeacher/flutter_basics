4a72dfd7eab270e50684e244467abe10da0e9e33
Stanislav Chernyshev <madmasm@gmail.com>
2025-03-03 23:16:59 +0300

    small fix

--- 4/tetris/lib/game_router.dart
+++ 4/tetris/lib/game_router.dart
@@ -1,17 +1,18 @@
 part of 'main.dart';
 
-///  Роутер игры.
+//  Роутер игры.
 abstract final class GameRouter {
-  /// Начальный маршрут.
+  // Начальный маршрут.
   static const String initialRoute = '/';
 
-  /// Маршрут игры
+  // Маршрут игры
   static const String gameRoute = '/game';
 
-  /// Маршрут окончания игры
+  // Маршрут окончания игры
   static const String gameOverRoute = '/game_over';
 
-  /// Маршруты приложения.
+  // Маршруты приложения. Объявляются приватными, чтобы 
+  // исключить к ним доступ вне навигатора
   static final Map<String, WidgetBuilder> _appRoutes = {
     // Рутовый экран - главное меню
     initialRoute: (_) => const MainMenuScreen(),
