9aa1903ddaa5c180a850ded2e25de94c935c8a1f
Yuri Petrov <48598325+petrovyuri@users.noreply.github.com>
2025-03-03 22:23:57 +0300

    feat(tetris_4): Добавить навигацию в игру (#18)
    * add screen
    
    * add router
    
    * add scores
    
    * fix
    
    * ref
    
    ---------
    
    Co-authored-by: petrovyuri <y.petrov@friflex.com>

---
+++ 4/tetris/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:tetris/screens/game_over_screen.dart';
+import 'package:tetris/screens/game_screen.dart';
+import 'package:tetris/screens/main_menu_screen.dart';
+
+part 'game_router.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: GameRouter.initialRoute,
+        routes: GameRouter._appRoutes);
+  }
+}
