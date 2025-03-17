49bc545604e846f43155212f103e8daffb05c26f
Stanislav Ilin <youngfrezyx@gmail.com>
2025-03-17 15:10:09 +0500

    Add all sqlite methods

--- 6/sqlite/lib/main.dart
+++ 6/sqlite/lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:sqlite/repository/user_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await _initDatabase();
-  await _addUser();
-  runApp(const MyApp());
+  final db = await _initDatabase();
+  runApp(MyApp(db: db));
 }
 
 /// Метод инициализации базы данных
-Future<void> _initDatabase() async {
+Future<Database> _initDatabase() async {
   final db = await openDatabase('app_database.db');
   await db.execute('''
   CREATE TABLE IF NOT EXISTS users (
@@ -19,143 +19,91 @@ Future<void> _initDatabase() async {
       age INTEGER
   );
   ''');
+  return db;
 }
-
-/// Метод создания нового пользователя
-Future<void> _addUser() async {
-  final db = await openDatabase('app_database.db');
-  await db.insert('users', {
-    'name': 'Елена',
-    'email': 'elena@gmail.com',
-    'age': 25,
-  });
-}
-
-/// Метод создания нового пользователя
-// Future<void> _addUser() async {
-//   final db = await openDatabase('app_database.db');
-//   await db.execute(
-//     'INSERT INTO users (name, email, age) VALUES (?, ?, ?)',
-//     ['Елена1', 'elena1@gmail.com', 25],
-//   );
-// }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+    required this.db,
+  });
 
-  // This widget is the root of your application.
+  final Database db;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Sqflite',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(db: db),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key, required this.db});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  final Database db;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeScreenState extends State<HomeScreen> {
+  late final UserRepository _userRepository;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  List<User> _users = [];
+
+  @override
+  void initState() {
+    _initRepository().then((_) => _loadUsers());
+    super.initState();
+  }
+
+  Future<void> _initRepository() async {
+    _userRepository = UserRepository(db: widget.db);
+    await _userRepository.init();
+  }
+
+  Future<void> _loadUsers() async {
+    final users = await _userRepository.getUsers();
+    if (mounted) {
+      setState(() => _users = users);
+    }
+  }
+
+  Future<void> _createNewUser() async {
+    await _userRepository.createUser(CreateUserDto(
+      name: 'Вадим',
+      email: 'vadim@example.com',
+      age: 55,
+    ));
+    await _loadUsers();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: _users.length,
+        itemBuilder: (context, index) {
+          final user = _users[index];
+          return ListTile(
+            leading: Text(user.id.toString()),
+            title: Text(user.name),
+            subtitle: Text(user.email),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons.add),
+        onPressed: () async {
+          await _createNewUser();
+        },
+      ),
     );
   }
 }
--- 6/sqlite/lib/repository/user_repository.dart
+++ 6/sqlite/lib/repository/user_repository.dart
@@ -18,8 +18,84 @@ class UserRepository {
     ''');
   }
 
+  /// Создание нового пользователя
   Future<void> createUser(CreateUserDto user) async {
     await db.insert('users', user.toMap());
+  }
+
+  /// Получение всех пользователей
+  Future<List<User>> getUsers() async {
+    final data = await db.query('users');
+    return data.map((e) => User.fromMap(e)).toList();
+  }
+
+  /// Получение пользователя по id
+  Future<User> getUser(int id) async {
+    final data = await db.query('users', where: 'id = ?', whereArgs: [id]);
+    if (data.isEmpty) throw Exception('User not found');
+    return User.fromMap(data.first);
+  }
+
+  Future<List<User>> getYoungUsers() async {
+    final data = await db.query('users', where: 'age < ?', whereArgs: [18]);
+    return data.map((e) => User.fromMap(e)).toList();
+  }
+
+  /// Обновление пользователя
+  Future<void> updateUser(User user) async {
+    await db.update(
+      'users',
+      user.toMap(),
+      where: 'id = ?',
+      whereArgs: [user.id],
+    );
+  }
+
+  /// Удаление пользователя
+  Future<void> deleteUser(int id) async {
+    await db.delete('users', where: 'id = ?', whereArgs: [id]);
+  }
+
+  /// Удаление всех пользователей
+  Future<void> deleteAllUsers() async {
+    await db.delete('users');
+  }
+}
+
+class User {
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.age,
+  });
+
+  final int id;
+  final String name;
+  final String email;
+  final int age;
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map['id'] as int,
+      name: map['name'] as String,
+      email: map['email'] as String,
+      age: map['age'] as int,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'email': email,
+      'age': age,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'User{id: $id, name: $name, email: $email, age: $age}';
   }
 }
 
@@ -49,4 +125,42 @@ class CreateUserDto {
       'age': age,
     };
   }
+}
+
+/// Метод создания нового пользователя
+Future<void> _addUser() async {
+  final db = await openDatabase('app_database.db');
+  await db.insert('users', {
+    'name': 'Елена',
+    'email': 'elena@gmail.com',
+    'age': 25,
+  });
+}
+
+/// Метод создания нового пользователя
+Future<void> _addUserBasic() async {
+  final db = await openDatabase('app_database.db');
+  await db.execute(
+    'INSERT INTO users (name, email, age) VALUES (?, ?, ?)',
+    ['Елена1', 'elena1@gmail.com', 25],
+  );
+}
+
+Future<void> _getUsersBasic() async {
+  final db = await openDatabase('app_database.db');
+  final data = await db.rawQuery('SELECT * FROM users');
+  print(data);
+}
+
+Future<void> _updateUserBasic(User user) async {
+  final db = await openDatabase('app_database.db');
+  await db.rawQuery(
+    "UPDATE users SET name = ?, email = ?, age = ? WHERE id = ?",
+    [user.name, user.email, user.age, user.id],
+  );
+}
+
+Future<void> _deleteUserBasic(int id) async {
+  final db = await openDatabase('app_database.db');
+  await db.rawDelete('DELETE FROM users WHERE id = ?', [id]);
 }
