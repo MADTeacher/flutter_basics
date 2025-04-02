3599db9e99af1473f2b3a35f646fccf8f54c6c8a
petrovyuri <y.petrov@friflex.com>
2025-04-02 20:52:54 +0300

    fix

--- 5/tetris/lib/features/game/game_over_screen.dart
+++ 5/tetris/lib/features/game/game_over_screen.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:tetris/app/context_ext.dart';
 import 'package:tetris/features/game/game_scores.dart';
+import 'package:tetris/features/user/domain/state/user_state.dart';
 import 'package:tetris/main.dart';
 
-import '../user/domain/state/user_state.dart';
-
-///  Экран окончания игры
+/// Экран окончания игры
 class GameOverScreen extends StatelessWidget {
   const GameOverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
         /// Слушатель состояния кубита пользователя
         /// и отображение соответствующего виджета
         /// в зависимости от состояния
@@ -23,10 +21,7 @@ class GameOverScreen extends StatelessWidget {
           UserSuccessState() => GameScores(
               score: state.userEntity.score,
               onRestart: () {
-                Navigator.pushReplacementNamed(
-                  context,
-                  GameRouter.gameRoute,
-                );
+                Navigator.pushReplacementNamed(context, GameRouter.gameRoute);
               }),
           _ => SizedBox.shrink(),
         };
