7bbbea957686d008ddfc5c3d464183b4a4d974bc
petrovyuri <y.petrov@friflex.com>
2025-04-02 19:37:48 +0300

    fix(_middleware): Добавить CORS

--- tetris_backend/routes/_middleware.dart
+++ tetris_backend/routes/_middleware.dart
@@ -4,6 +4,14 @@ import 'package:talker_dart_frog_logger/talker_dart_frog_logger.dart';
 Handler middleware(Handler handler) {
   return (context) async {
     final talker = context.read<Talker>();
-    return loggerMiddleware(handler: handler, talker: talker)(context);
+    final response = await loggerMiddleware(handler: handler, talker: talker)(context);
+    return response.copyWith(
+      headers: {
+        ...response.headers,
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+    );
   };
 }
